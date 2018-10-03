e4327680346dea3bd851238b1ba32684d6b7ffb4
hi <sophia.dowlatabadi@st.ovgu.de>
2018-10-03 13:08:10 +0200

    navbar
    navbar.php hinzugefügt + in index.php integriert

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-	echo "Hi";
-?>
+<?php echo "Hi"; ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="css/style.css">
+	<?php include('navbar.php'); ?>
+</head>
+
+<body>
+</body>
+
+</html>
