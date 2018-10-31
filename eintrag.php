3de847dd8d82ea7aab45625baf449625bf0e9124
hi <sophia.dowlatabadi@st.ovgu.de>
2018-10-31 23:03:39 +0100

    Datenbankenanpassungen

--- eintrag.php
+++ eintrag.php
@@ -1,3 +1,11 @@
+<?php
+	include_once 'includes/dbh.inc.php';
+
+	$sql = "SELECT * FROM azubi;";
+	$result = mysqli_query($conn, $sql);
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -15,46 +23,62 @@
 	<div class="card bg-light text-dark">
 	  <div class="card-body">
 
-	  		<div class="input-group mb-3">
-			  <div class="input-group-prepend">
-			    <label class="input-group-text" for="inputGroupSelectName">Name</label>
-			  </div>
-			  <select class="custom-select" id="inputGroupSelectName">
-			    <option selected>Wähle deinen Namen aus...</option>
-			    <option value="1">One</option>
-			    <option value="2">Two</option>
-			    <option value="3">Three</option>
-			  </select>
-			</div>
+	  	<form>
 
-			<div class="input-group mb-3">
-			  <div class="input-group-prepend">
-			    <label class="input-group-text" for="inputGroupSelectKürzel">Kürzel</label>
-			  </div>
-			  <select class="custom-select" id="inputGroupSelectKürzel">
-			    <option selected>Wähle dein Kürzel aus...</option>
-			    <option value="1">One</option>
-			    <option value="2">Two</option>
-			    <option value="3">Three</option>
-			  </select>
-			</div>
+		  		<div class="input-group mb-3">
+			          <div class="input-group-prepend">
+			            <label class="input-group-text" for="inputGroupSelectName">Name</label>
+			          </div>
+			         <select class="custom-select" id="inputGroupSelectName">
+			         	<option selected>Wähle deinen Namen aus...</option>
+			           <?php $result = mysqli_query($conn, "SELECT * FROM azubi");
+			           //alle Zeilen aus der Abfrage in ein Array packen
+			           $resultset = array();
+			           while ($row = mysqli_fetch_array($result)) {
+			             $resultset[] = $row;
+			           }
+			           //die Einträge im Array durchgehen (Jeder Eintrag entspricht einer Zeile in der Tabelle aus deiner Query)
+			           //Für jeden Eintrag wird eine Option zu dem select hinzugefügt
+			           foreach ($resultset as $row) : ?>
 
-
-			<!--hier kommen die 2 Datumsfelder hin-->
+			            <option value = "<?php echo $row['name']?>"><?php echo $row['name']?></option>
+			           <?php endforeach ?> 
+			          </select>
+			        </div>
 
 
-			<div class="input-group mb-3">
-			  <div class="input-group-prepend">
-			    <label class="input-group-text" for="inputGroupSelectGrund">Grund</label>
-			  </div>
-			  <select class="custom-select" id="inputGroupSelectGrund">
-			    <option selected>Wähle dein Kürzel aus...</option>
-			    <option value="1">One</option>
-			    <option value="2">Two</option>
-			    <option value="3">Three</option>
-			  </select>
-			</div>
-		<button type="button" class="btn btn-success btn-lg btn-block">Speichern</button>	
+				<div class="input-group mb-3">
+				  <div class="input-group-prepend">
+				    <label class="input-group-text" for="inputGroupSelectKürzel">Kürzel</label>
+				  </div>
+				  <select class="custom-select" id="inputGroupSelectKürzel">
+				    <option selected>Wähle dein Kürzel aus...</option>
+				    <option value="1">One</option>
+				    <option value="2">Two</option>
+				    <option value="3">Three</option>
+				  </select>
+				</div>
+
+
+				<!--hier kommen die 2 Datumsfelder hin-->
+
+
+				<div class="input-group mb-3">
+				  <div class="input-group-prepend">
+				    <label class="input-group-text" for="inputGroupSelectGrund">Grund</label>
+				  </div>
+				  <select class="custom-select" id="inputGroupSelectGrund">
+				    <option selected>Wähle dein Kürzel aus...</option>
+				    <option value="1">One</option>
+				    <option value="2">Two</option>
+				    <option value="3">Three</option>
+				  </select>
+				</div>
+
+				<button type="button" class="btn btn-success btn-lg btn-block">Speichern</button>	
+
+		</form>
+
 	</div>
 	</div>
 	<footer>
