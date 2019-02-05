b4035bdfb1e1ddd323058517baf914627e50d396
hi <sophia.dowlatabadi@st.ovgu.de>
2019-02-05 19:01:57 +0100

    tabelle

--- uebersicht.php
+++ uebersicht.php
@@ -10,6 +10,20 @@ function dates_month($month, $year) {
 
     for ($i = 1; $i <= $num; $i++) {
         $mktime = mktime(0, 0, 0, $month, $i, $year);
+        $date = date("Y-m-d", $mktime);
+        $dates_month[$i] = $date;
+    }
+
+    return $dates_month;
+}
+
+function date_month($month, $year) {
+		date_default_timezone_set('Europe/Berlin');
+    $num = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    $dates_month = array();
+
+    for ($i = 1; $i <= $num; $i++) {
+        $mktime = mktime(0, 0, 0, $month, $i, $year);
         $date = date("D d", $mktime);
         $dates_month[$i] = $date;
     }
@@ -35,8 +49,10 @@ function EintraegeFuerUebersicht(){
 	$EintraegeFuerUebersicht = mysqli_query($conn,"SELECT * FROM eintrag;");
 	$Eintraege = Array();
 	while ($row = mysqli_fetch_array($EintraegeFuerUebersicht, MYSQLI_ASSOC)) {
-	$Eintraege[] =  $row['name'].$row['grund'].$row['tageszeit'].$row['anfangEins'].$row['endeEins'].$row['Von'].$row['Bis']; 
+	$Eintraege[] =  $row;
 }
+	//echo $Eintraege[0]['name']; // test; mit for schleife alle 
+
 	return $Eintraege;
 }
 
@@ -46,19 +62,21 @@ function SophiasKalender($month,$year){
      $TageDesMonats = array();
      $TageDesMonats = dates_month($month, $year);
 
+     $TageDesMonat = array();
+     $TageDesMonat = date_month($month, $year);
+
      $Azubis= Array();
      $Azubis = AlleAzubisUndDualeStudenten();
 
      $Eintraege = Array();
      $Eintraege = EintraegeFuerUebersicht();
 
-
      $calendar = "<table class='table-bordered table-responsive'>";//table table-bordered table-sm
      $calendar  .= "<th>Azubis/Duale Studenten</th>";
 
-     for($i = 1; $i <= count($TageDesMonats); $i++) { 
+     foreach ($TageDesMonat as $tag) {
 
-          $calendar .= "<th class='header'>$TageDesMonats[$i]</th>"; //// ich könnte jeder row die id geben mit dem Tag des Monats
+           $calendar .= "<th class='header'>$tag</th>";
      }
 
      for($i = 0; $i < count($Azubis); $i++)
@@ -66,19 +84,25 @@ function SophiasKalender($month,$year){
           $calendar  .= "<tr>"; // ich könnte jeder row die id geben mit dem namen des Azubis
           $calendar  .= "<td>" . $Azubis[$i] . "</td>";
           foreach ($TageDesMonats as $tag) {
-          	//if($Eintraege['anfangEins'] = $tag){*/
-          	$calendar  .= "<td></td>";
-          	/*}
-          	else{
-          	$calendar  .= "<td></td>";
-          }*/
+          	$found = false;
+          	for($k = 0; $k < count($Eintraege); $k++){
+          			if($Azubis[$i] == $Eintraege[$k]['name']){
+          				if(( $Eintraege[$k]['anfangEins']<= $tag) && ($Eintraege[$k]['endeEins'] >= $tag )){
+          					$calendar  .= "<td align='center' class ='day event event-urlaub'>".$Eintraege[$k]['grund']."</td>";
+          					$found = true;
+          			}
+          	}
+          }
+          	if($found == false){
+          		$calendar  .= "<td></td>";
+          	}
           }
           $calendar  .= "</tr>";
      }
 
      return $calendar; 
-
 }
+
 
 ?>
 
